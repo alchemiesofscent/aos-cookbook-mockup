c8fae99420b9ce08d9282952fe7675a70c692f92
alchemiesofscent <coughlin@flu.cas.cz>
2026-01-15 12:13:54 +0100

    Move hardcoded main data into seed.json

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       resolve: {
         alias: {
           '@': path.resolve(__dirname, 'src'),
+          '@seed.json': path.resolve(__dirname, 'src/seed.json'),
         }
       }
     };
